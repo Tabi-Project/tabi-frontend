184cbf549e312ba0b2979761fad5f6205467b727
glowwEE <gloryokafor5@gmail.com>
2026-03-14 12:22:15 +0000

    deleted resources page

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
+const repo = "/tabi-frontend";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
-  basePath: isProd ? "/tabi-frontend" : "",
-  assetPrefix: isProd ? "/tabi-frontend/" : "",
+  basePath: isProd ? repo : "",
+  assetPrefix: isProd ? `${repo}/` : "",
+  trailingSlash: true,
   images: {
     unoptimized: true
   },
-  trailingSlash: true,
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/tabi-frontend" : ""
+    NEXT_PUBLIC_BASE_PATH: isProd ? repo : ""
   }
 };
 
